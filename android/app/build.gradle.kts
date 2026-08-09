d4d9758b1d191a7d222323dd0a3be6a00eb157b3
nabilkencana <nabilkencana20@gmail.com>
2026-08-09 11:38:24 +0700

    Add Firebase config files google-services.json and GoogleService-Info.plist

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.eppos_both"
+    namespace = "com.example.epposBoth"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -17,7 +17,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.eppos_both"
+        applicationId = "com.example.epposBoth"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
